3550e8bed88bf68f701204e8e65ebda8aebb7a07
Zach Klabunde <zbunde@gmail.com>
2014-03-18 10:08:18 -0600

    first commit, finished first 5 tests

---
+++ bob.rb
@@ -0,0 +1,28 @@
+class Bob
+  def hey (string)
+
+  if string.upcase == string
+    "Woah, chill out!"
+
+  elsif string.end_with? ('?')
+    "Sure."
+
+  elsif string.empty?
+    "Fine. Be that Way!"
+
+  elsif string == "Let's go make out behind the gym!"
+    "Whatever."
+
+  elsif string == "It's OK if you don't what to go to the DMV."
+    "Whatever."
+
+  elsif string == string.upcase.end_with?('')
+    "Woah, chill out!"
+
+  else
+    "Whatever."
+
+
+  end
+  end
+end
